7a91e4e8488c4073815769b6336fb914517a6adc
Prateek <prateek_soni@outlook.com>
2022-01-09 01:38:48 +0530

    add social links

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,6 +2,7 @@ import { ContactFormFields } from "@/types";
 import { FC } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { toast } from "react-toastify";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Contact: FC = () => {
   const { register, handleSubmit } = useForm<ContactFormFields>();
@@ -24,35 +25,54 @@ const Contact: FC = () => {
 
   return (
     <div id="contact" className="container m-auto py-12">
-      <h2 className="text-6xl text-center font-bold text-primary-light">
-        Contact me
-      </h2>
       <div className="mt-12 p-8 bg-primary-dark rounded-lg text-white">
-        <div className="grid grid-cols-2">
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <label htmlFor="name">
-              <div className="text-lg mb-2">Your name</div>
-              <input
-                className="p-2 bg-dark w-full rounded"
-                type="text"
-                {...register("name", {
-                  required: "Name cannot be empty",
-                })}
-              />
-            </label>
-            <label className="block mt-4" htmlFor="message">
-              <div className="text-lg mb-2">Your message</div>
-              <textarea
-                className="p-2 bg-dark w-full rounded"
-                {...register("message", {
-                  required: "Message cannot be empty",
-                })}
-              />
-            </label>
-            <button className="mt-4 font-bold px-8 py-3 bg-primary rounded-full">
-              Send
-            </button>
-          </form>
+        <h2 className="text-6xl text-center font-bold text-primary-light">
+          Contact me
+        </h2>
+        <div className="mt-8 grid grid-cols-2">
+          <div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <label htmlFor="name">
+                <div className="text-lg mb-2">Your name</div>
+                <input
+                  className="p-2 bg-dark w-full rounded"
+                  type="text"
+                  {...register("name", {
+                    required: "Name cannot be empty",
+                  })}
+                />
+              </label>
+              <label className="block mt-4" htmlFor="message">
+                <div className="text-lg mb-2">Your message</div>
+                <textarea
+                  className="p-2 bg-dark w-full rounded"
+                  {...register("message", {
+                    required: "Message cannot be empty",
+                  })}
+                />
+              </label>
+              <button className="mt-4 font-bold px-8 py-3 bg-primary rounded-full">
+                Send
+              </button>
+            </form>
+            <div className="uppercase text-xl mt-8">or</div>
+            <div className="mt-8 flex items-center gap-8">
+              <a
+                href="https://www.linkedin.com/in/prateeksoni47/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaLinkedin size={40} />
+              </a>
+              <a
+                href="https://github.com/prateeksonii"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaGithub size={40} />
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
