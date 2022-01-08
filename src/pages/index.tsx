3a4de5c24560819ce4e87f22dc84a14f6659f4aa
Prateek <prateek_soni@outlook.com>
2022-01-09 01:43:34 +0530

    update logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,17 +3,23 @@ import Contact from "@components/Contact";
 import Experiences from "@components/Experiences";
 import Hero from "@components/Hero";
 import Projects from "@components/Projects";
+import Head from "next/head";
 import { FC } from "react";
 
 const IndexPage: FC = () => {
   return (
-    <div className="h-screen">
-      <Hero />
-      <Projects />
-      <Experiences />
-      <About />
-      <Contact />
-    </div>
+    <>
+      <Head>
+        <title>Prateek Soni</title>
+      </Head>
+      <div className="h-screen">
+        <Hero />
+        <Projects />
+        <Experiences />
+        <About />
+        <Contact />
+      </div>
+    </>
   );
 };
 
